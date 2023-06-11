6703eac49fee9f56f8a82a70ab0bf6055fb12f5c
afam22 <happyguy_afam@yahoo.com>
2023-06-10 21:26:06 -0600

    Initial commit for banking app project

---
+++ src/main/java/com/anabank/anabank/AnabankApplication.java
@@ -0,0 +1,13 @@
+package com.anabank.anabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnabankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnabankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anabank/anabank/AnabankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anabank.anabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnabankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
