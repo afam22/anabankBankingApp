0327eef87049bbfc5109d46e9ccc93401d4c0977
afam22 <happyguy_afam@yahoo.com>
2023-06-10 23:08:18 -0600

    created a Data Transfer Object Dto class 2

---
+++ src/main/java/com/anabank/anabank/dto/AccountInfo.java
@@ -0,0 +1,16 @@
+package com.anabank.anabank.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AccountInfo {
+    private String accountName;
+    private String accountBalance;
+    private String accountNumber;
+}
