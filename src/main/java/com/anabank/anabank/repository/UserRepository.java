1739c5ff29eefaaf3738e3bff6c39e6791aa3806
afam22 <happyguy_afam@yahoo.com>
2023-06-10 22:51:08 -0600

    created UserRepository interface that accesses the JPA repository methods for talking to the database.

---
+++ src/main/java/com/anabank/anabank/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.anabank.anabank.repository;
+
+import com.anabank.anabank.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/* provide methods for talking to database.
+    manipulates the user table and its corresponding entity
+    by referencing the primary key id(Long)
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+}
