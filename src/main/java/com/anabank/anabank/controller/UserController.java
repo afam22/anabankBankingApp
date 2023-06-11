56f3257b074d4ab36f25987b5054a709560b7b45
afam22 <happyguy_afam@yahoo.com>
2023-06-11 02:54:09 -0600

    updated

---
+++ src/main/java/com/anabank/anabank/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.anabank.anabank.controller;
+
+import com.anabank.anabank.dto.BankResponse;
+import com.anabank.anabank.dto.UserRequest;
+import com.anabank.anabank.service.impl.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping
+    public BankResponse createAccount(@RequestBody UserRequest userRequest){
+        return userService.createAccount(userRequest);
+    }
+}
