d6b6aeebaebc60d1f1cb2aa5d860ba5908e4129a
afam22 <happyguy_afam@yahoo.com>
2023-06-10 23:34:58 -0600

    created a generate account number method in Account Util Class and added it to the util package.

---
+++ src/main/java/com/anabank/anabank/utils/AccountUtils.java
@@ -0,0 +1,30 @@
+package com.anabank.anabank.utils;
+
+import java.time.Year;
+
+public class AccountUtils {
+    /*
+    For my account number builder assuming i have a 10-digit account number
+    I want to build an account stating with 2023 + randomSixDigits
+     */
+
+    public static String generateAccountNumber(){
+        Year currentYear = Year.now();
+        int min = 100000;           //minimum 6-digit number starting with 1
+        int max = 999999;           //maximum 6-digit number ending in 999999
+
+        //generating a random number between min and max
+        int randNumber = (int)Math.floor(Math.random() * (max - min + 1) + min);
+
+        //convert the current and randomNumber to Strings, then concatenate them
+
+        String year = String.valueOf(currentYear);
+        String randomNumber = String.valueOf(randNumber);
+
+        StringBuilder accountNumber = new StringBuilder();
+
+        return accountNumber.append(year).append(randomNumber).toString();
+
+    }
+
+}
